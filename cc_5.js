f31a1c9c03b36237faf0d0b845b105ca06e6f64f
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-07 15:04:51 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,16 @@
+// Task 1: Object Properties
+
+
+let customer = { // these lines declare an object with 3 properties
+    name: "LeBron James", 
+    age: 40, 
+    email: "king.james@email.com" 
+};
+
+
+// the lines below log each of the objects properties in the console
+console.log(`The customer's name is: ${customer.name}`);
+console.log(`The customer's age is: ${customer.age}`);
+console.log(`The customer's email is: ${customer.email}`);
+
+
